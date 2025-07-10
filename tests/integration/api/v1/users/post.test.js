c9870971d9eeeda1b28e96689ca5019f6845a72a
Alex <alexgithub94@gmail.com>
2025-07-10 06:08:06 -0300

    feat: add insert de user no banco de dados

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,5 @@
+import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator.js";
-import database from "infra/database";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -10,17 +10,34 @@ beforeAll(async () => {
 describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
     test("With unique and valid data", async () => {
-      await database.query({
-        text: "INSERT INTO users (username, email, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)",
-        values: ["alexdesouza", "alex94tu@gmail.com", "senha123"],
-      });
-      const users = await database.query("SELECT * FROM users");
-
-      console.log(users.rows);
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // Pega objeto javascript e converte para uma string em json.
+        body: JSON.stringify({
+          username: "alexdesouza",
+          email: "alex94tu@gmail.com",
+          password: "senha123",
+        }),
       });
+
       expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "alexdesouza",
+        email: "alex94tu@gmail.com",
+        password: "senha123",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
     });
   });
 });
