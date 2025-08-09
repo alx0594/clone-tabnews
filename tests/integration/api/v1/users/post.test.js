9c609260a5b92eadf6b26ab5723281c40821994d
Alex <alexgithub94@gmail.com>
2025-08-09 08:56:15 -0300

    fix: update msg de error

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -93,7 +93,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O email informado já está sendo utilizado.",
-        action: "Utilize outro e-mail para realizar esta operação.",
+        action: "Utilize outro email para realizar esta operação.",
         status_code: 400,
       });
     });
